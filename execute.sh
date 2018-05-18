fb272cb2125070a6c02359d7de036a7ea6b52e4f
kinjo yuji <kinjoyuji@gmail.com>
2018-05-19 02:25:21 +0900

    stop spring

--- execute.sh
+++ execute.sh
@@ -1,3 +1,4 @@
+bundle exec rails app:template LOCATION=https://raw.githubusercontent.com/Cam1no/RailsReactTemplate/master/stop_spring.rb
 bundle exec rails app:template LOCATION=https://raw.githubusercontent.com/Cam1no/RailsReactTemplate/master/rubocop.rb
 bundle exec rails app:template LOCATION=https://raw.githubusercontent.com/Cam1no/RailsReactTemplate/master/locale.rb
 bundle exec rails app:template LOCATION=https://raw.githubusercontent.com/Cam1no/RailsReactTemplate/master/pry.rb
