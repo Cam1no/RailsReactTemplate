18022a6e06c633bc5a75c555cf5a6db68fd874dd
kinjo yuji <kinjoyuji@gmail.com>
2018-05-19 01:21:23 +0900

    localの設定

---
+++ local.rb
@@ -0,0 +1,4 @@
+# install locales
+remove_file 'config/locales/en.yml'
+run 'wget https://raw.github.com/svenfuchs/rails-i18n/master/rails/locale/en.yml -P config/locales/'
+run 'wget https://raw.github.com/svenfuchs/rails-i18n/master/rails/locale/ja.yml -P config/locales/'
