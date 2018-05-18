65eabd7db485807e252c964c7c46cb2af2f6c2de
kinjo yuji <kinjoyuji@gmail.com>
2018-05-19 02:36:50 +0900

    capistrano

---
+++ capistrano.rb
@@ -0,0 +1,12 @@
+gem_group :development do
+  gem 'capistrano'
+  gem 'capistrano-bundler'
+  gem 'capistrano-rails'
+  gem 'capistrano-rbenv'
+  gem 'capistrano-yarn'
+  gem 'capistrano3-unicorn'
+end
+
+run_bundle
+
+run 'bundle exec cap install'
--- execute.sh
+++ execute.sh
@@ -4,3 +4,4 @@ bundle exec rails app:template LOCATION=https://raw.githubusercontent.com/Cam1no
 bundle exec rails app:template LOCATION=https://raw.githubusercontent.com/Cam1no/RailsReactTemplate/master/pry.rb
 bundle exec rails app:template LOCATION=https://raw.githubusercontent.com/Cam1no/RailsReactTemplate/master/kaminari.rb
 bundle exec rails app:template LOCATION=https://raw.githubusercontent.com/Cam1no/RailsReactTemplate/master/erb2slim.rb
+bundle exec rails app:template LOCATION=https://raw.githubusercontent.com/Cam1no/RailsReactTemplate/master/capstrano.rb
