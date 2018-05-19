214231e150cbe7014c6cd922de2a054fe51813f3
kinjo yuji <kinjoyuji@gmail.com>
2018-05-19 11:44:45 +0900

    eslintの設定

---
+++ eslint.rb
@@ -0,0 +1,4 @@
+run 'yarn add babel-eslint eslint eslint-plugin-class-property'
+run 'yarn add eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react --dev'
+
+run 'svn export https://github.com/Cam1no/RailsReactTemplate/trunk/files/eslintrc.js ./.eslint.js'
