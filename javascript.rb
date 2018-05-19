3b84ac65eeef00a68ea02cdafe4ec635a0e990d3
kinjo yuji <kinjoyuji@gmail.com>
2018-05-19 10:44:00 +0900

    yarn コマンド追記

--- javascript.rb
+++ javascript.rb
@@ -6,6 +6,14 @@ run 'svn export https://github.com/Cam1no/RailsReactTemplate/trunk/javascript/ .
 
 run 'yarn add redux react-redux redux-thunk react-router-dom redux-devtools redux-devtools-dock-monitor redux-devtools-log-monitor redux-slider-monitor'
 
+insert_into_file 'package.json', before: '"dependencies": {' do
+  <<-JSON
+"scripts": {
+  "dev": "NODE_ENV=development ENVIRONMENT=development ./bin/webpack-dev-server --watch --colors --progress"
+},
+  JSON
+end
+
 git add: "."
 
 git commit: %Q{ -m 'Reactの環境構築' }
