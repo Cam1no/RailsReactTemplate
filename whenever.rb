b55de5893909028b9aab26c56a2ef9fe6f04a625
kinjo yuji <kinjoyuji@gmail.com>
2018-05-26 12:01:14 +0900

    config/schedule.rbの設定を追記

--- whenever.rb
+++ whenever.rb
@@ -4,6 +4,19 @@ run_bundle
 
 run 'bundle exec wheneverize .'
 
+insert_into_file 'config/schedule.rb', after: "Learn more: http://github.com/javan/whenever\n" do
+  <<~RUBY
+  set :output, 'log/crontab.log'
+  set :environment, :production
+
+  every 1.minutes do
+    Rails.logger.debug '-------------'
+    Rails.logger.debug 'Whenever実行中'
+    Rails.logger.debug '-------------'
+  end
+  RUBY
+end
+
 git add: "."
 
 git commit: %Q{ -m 'wheneverの設定' }
