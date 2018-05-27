687a0239e22900373fe84ff3bddccb91ec5a416a
kinjo yuji <kinjoyuji@gmail.com>
2018-05-27 12:21:30 +0900

    Route層でのエラーをキャッチするように

--- error_handling.rb
+++ error_handling.rb
@@ -1,15 +1,6 @@
 insert_into_file 'app/controllers/application_controller.rb', after: "class ApplicationController < ActionController::API\n" do
   <<-RUBY
   rescue_from StandardError, with: :internal_server_error unless Rails.env.development?
-  unless Rails.env.development?
-    rescue_from(NoMemoryError,
-                ScriptError,
-                Interrupt,
-                SecurityError,
-                SignalException,
-                SystemExit,
-                SystemStackError, with: :only_logging)
-  end
   rescue_from ActiveRecord::RecordNotFound, with: :not_found unless Rails.env.development?
 
   private
@@ -23,12 +14,17 @@ insert_into_file 'app/controllers/application_controller.rb', after: "class Appl
     return render json: { message: 'Record Not Found' }, status: :not_found if error.nil?
     render json: { message: error.full_message }, status: :not_found
   end
-
-  def only_logging(error = nil)
-    # TODO
-    # need emergency notify
-    Rails.logger.warn('System Error') if error.nil?
-    Rails.logger.warn(error.full_message)
-  end
   RUBY
 end
+
+create_file 'config/initializers/exceptions_app.rb', <<-RUBY
+Rails.configuration.exceptions_app = ->(env) { ErrorsController.action(:error).call(env) }
+RUBY
+
+create_file 'app/controllers/errors_controller.rb', <<-RUBY
+class ErrorsController < ActionController::API
+  def error
+    raise env['action_dispatch.exception']
+  end
+end
+RUBY
