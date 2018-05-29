0229d3fdf8021d81108755a0d64fe7a43f5fb807
kinjo yuji <kinjoyuji@gmail.com>
2018-05-29 12:25:22 +0900

    prydebugのエイリアス追加

--- pry.rb
+++ pry.rb
@@ -22,6 +22,14 @@ begin
 rescue LoadError
   puts "no awesome_print :("
 end
+
+# prydebug
+if defined?(PryByebug)
+  Pry.commands.alias_command 's', 'step'
+  Pry.commands.alias_command 'n', 'next'
+  Pry.commands.alias_command 'f', 'finish'
+  Pry.commands.alias_command 'c', 'continue'
+end
 RUBY
 
 git add: "."
