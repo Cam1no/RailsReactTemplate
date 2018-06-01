9df8f9d6f6c6a7dcdc661093b3492b14617f1553
kinjo yuji <kinjoyuji@gmail.com>
2018-06-01 22:18:47 +0900

    fix bugs

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-bundle exec rails app:template LOCATION=https://raw.githubusercontent.com/Cam1no/RailsReactTemplate/master/application.rb
 bundle exec rails app:template LOCATION=https://raw.githubusercontent.com/Cam1no/RailsReactTemplate/master/javascript.rb
 bundle exec rails app:template LOCATION=https://raw.githubusercontent.com/Cam1no/RailsReactTemplate/master/stop_spring.rb
 bundle exec rails app:template LOCATION=https://raw.githubusercontent.com/Cam1no/RailsReactTemplate/master/locale.rb
+bundle exec rails app:template LOCATION=https://raw.githubusercontent.com/Cam1no/RailsReactTemplate/master/application.rb
 bundle exec rails app:template LOCATION=https://raw.githubusercontent.com/Cam1no/RailsReactTemplate/master/pry.rb
 bundle exec rails app:template LOCATION=https://raw.githubusercontent.com/Cam1no/RailsReactTemplate/master/kaminari.rb
 bundle exec rails app:template LOCATION=https://raw.githubusercontent.com/Cam1no/RailsReactTemplate/master/erb2slim.rb
