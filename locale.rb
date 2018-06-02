0839fdbb4b5f79e55b184a408c2ddb4d666793d8
kinjo yuji <kinjoyuji@gmail.com>
2018-06-02 15:53:16 +0900

    fix

--- locale.rb
+++ locale.rb
@@ -1,5 +1,6 @@
 # install locales
 gem 'rails-i18n'
+run_bundle
 remove_file 'config/locales/en.yml'
 run 'wget https://raw.github.com/svenfuchs/rails-i18n/master/rails/locale/en.yml -P config/locales/'
 run 'wget https://raw.github.com/svenfuchs/rails-i18n/master/rails/locale/ja.yml -P config/locales/'
