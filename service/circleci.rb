b6d156d31e501cca5deac073be3f24c6a30451ab
joe <kinjoyuji@gmail.com>
2018-08-17 02:46:03 +0900

    fix database setting

--- service/circleci.rb
+++ service/circleci.rb
@@ -70,28 +70,6 @@ jobs:
             RAILS_ENV=test bundle exec rspec
 YAML
 
-remove_file 'config/database.yml'
-
-create_file 'config/database.yml', <<-YAML
-default: &default
-  adapter: mysql2
-  pool: <%= ENV.fetch("RAILS_MAX_THREADS") { 5 } %>
-  username: <%= ENV['DB_USERNAME'] || 'root' %>
-  password: <%= ENV['DB_PASSWORD'] || 'root' %>
-  host: <%= ENV['DB_HOST'] || 'localhost' %>
-  port: 3306
-  encoding: utf8mb4
-  socket: /tmp/mysql.sock
-
-development:
-  <<: *default
-  database: #{Rails.application.class.parent_name}_development
-
-test:
-  <<: *default
-  database: #{Rails.application.class.parent_name}_test
-YAML
-
 git add: '.'
 
 git commit: %( -m 'setting circleci' )
